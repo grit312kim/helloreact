8c9a50e22df6a75ea0bcb1ba15acc27eeb45dd00
j312kim <grit312kim@gmail.com>
2019-05-07 23:57:36 +0900

    예제 작성

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Hello/>
+        <Hi/>
+        <Hello2 name='홍길동'
+          score='50'
+          />
+        <Hello2 name='홍길순'
+          score='70'
+          />
+        <Hi2 name='아무개'/>
+      </div>
+    );
+  }
+}
+
+class Hello extends React.Component{
+  render(){
+    return(
+      <div>hello</div>
+    );
+  }
+}
+class Hello2 extends React.Component{
+  constructor(){
+    super()
+    console.log('Hello2 constructor')
+    this.state = {
+      score:'30'
+    }
+  }
+  componentWillMount(){
+    console.log('Hello2 componentWillMount')
+    this.setState(
+      {
+        score:this.props.score
+      }
+    )
+  }
+  render(){
+    console.log('Hello2 render')
+    return(
+      <div>
+        hello2, {this.props.name}
+        {this.state.score}점       
+      </div>
+    );
+  }
+}
+
+const Hi = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Hi</div>
+  )
+}
+const Hi2 = (props) =>{
+  return (
+    <div>Hi2, {props.name}</div>
+  )
 }
 
 export default App;
